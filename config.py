010a4702bd450f62633ca0b392193f29686c7734
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2026-06-02 18:30:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+BOT_TOKEN = "6320753534:AAHmrh_Nr5_T1X1IJnjXn9k9RIskH1fXnys"  # @BotFather se lo
+CHANNEL_ID = "-1002785724070"  # Auto post ke liye channel
+ADMIN_ID = 6687248633  # Tumhara Telegram user ID (int)
+
+# Check interval in hours
+CHECK_INTERVAL_HOURS = 2
+
+# Pastys setup (agar tumhara khud ka hai to)
+PASTYS_API = "https://pastys.pages.dev/api/create"  # Adjust as per your pastys
